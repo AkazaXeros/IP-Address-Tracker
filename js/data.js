8becc655a7e29135c90773a9f7c272063be20db2
Wi-Go <akaza.xeros@gmail.com>
2023-07-04 09:44:24 +0200

    feat(Creation of the map using the documentation. First mock-up)

--- js/data.js
+++ js/data.js
@@ -1,1 +1,3 @@
+'use strict';
+
 export const apiKey = 'at_qR53sYQlw1PidbgV6QqlwUq4vdOb0';
--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+'use strict';
+
 import { apiKey } from './data.js';
+import { getData } from './assets.js';
 
 const requestedIP = '127.0.0.1'; // Example IP
 
-let fetchRequest = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${requestedIP}`;
+let urlIP = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${requestedIP}`;
+
+// DOM elements
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
