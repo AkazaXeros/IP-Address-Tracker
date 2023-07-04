5dc4e9fada14abb505746acc89041e10ebb1ff77
David <davidrguezglez933@gmail.com>
2023-07-04 17:57:39 +0200

    First tries with the API + problems finding IP Adress

--- js/data.js
+++ js/data.js
@@ -1,3 +1,20 @@
-'use strict';
+"use strict";
 
-export const apiKey = 'at_qR53sYQlw1PidbgV6QqlwUq4vdOb0';
+export const apiKey = "at_qR53sYQlw1PidbgV6QqlwUq4vdOb0";
+
+//192.168.1.178 IPv4 Address Will
+//100.90.1.1  Ipv4 DNS server
+//100.100.1.1 Ipv4 DNS server
+
+//192.168.0.105 IPv4 Address David
+//192.168.0.1 Ipv4 DNS server
+//92.176.65.17 Ip David Pagina
+
+//92.176.65.17
+
+//fe80::eca6:850:dfac:347a%8
+
+// Testing new GEO API
+//http://api.ipstack.com/192.168.0.105?access_key=5179bbcdcaa422d343501f4858fac531
+
+// "fe80::7e5e:d40e:2038:d59%14"
--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
-'use strict';
+"use strict";
 
-import { apiKey } from './data.js';
-import { getData } from './assets.js';
+import { apiKey } from "./data.js";
+import { getData } from "./assets.js";
 
-const requestedIP = '127.0.0.1'; // Example IP
+const requestedIP = "192.168.1.178"; // Example IP
 
-let urlIP = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${requestedIP}`;
+let urlIP = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`;
 
 // DOM elements
+const accordionElement = document.querySelector(".accordionBubble");
 
-const map = L.map('map').setView([51.505, -0.09], 13);
+// DOM elements from leafletjs source
+const map = L.map("map").setView([51.505, -0.09], 13);
+const marker = L.marker([40.4165, -3.70256], { alt: "marker" }).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// Event Listeners
+
+accordionElement.addEventListener("click", () => {
+	accordionElement.classList.toggle("is-opened");
+});
+
+const start = async () => {
+	try {
+		const data = await getData(urlIP);
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// start();
